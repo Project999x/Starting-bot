6d4ddf7bb05044bf652639715b423f821cbf5029
Sama <105520683+Project999x@users.noreply.github.com>
2025-10-20 11:06:22 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -3,8 +3,7 @@ import os
 import json
 
 # Bot credentials
-API_ID = int(os.getenv("API_ID", "19822764
-"))  # replace with your own
+API_ID = int(os.getenv("API_ID", "19822764"))  # replace with your own
 API_HASH = os.getenv("API_HASH", "b240e413364b8608a542a7cafc6903be")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8311348398:AAGy02UrCSC7eFRTt1lVg0yvuvIxWi7u0Pc")
 
